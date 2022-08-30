7ca8be61da0b689378deb66c45d67cea8fe87a4a
Elżbieta Janus-Kaleta <60756867+ElaJK01@users.noreply.github.com>
2022-08-31 01:04:36 +0200

    partly draggable continentDetails

--- src/pages/continentDetails.js
+++ src/pages/continentDetails.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import { map, path, prop } from "ramda";
 import styled from "styled-components";
 import Section from "../components/section";
@@ -29,45 +29,54 @@ const ContinentDetails = withLoadingData((props) => {
   const continentDetails = props |> path(["data", "continent"]);
   const countries = continentDetails |> path(["countries"]);
 
+  const ref = useRef(null);
+
   const itemsList = [
-    { id: 1, tag: <h3>{continentDetails.name}</h3>},
-    { id: 2, tag: <p>Code: {continentDetails.code}</p>},
+    { id: 1, tag: (<div style={{ border: "1px solid blue", margin: 2 }}><h3>{continentDetails.name}</h3></div>) },
+    { id: 2, tag: (<div style={{ border: "1px solid blue", margin: 2 }}><p>Code: {continentDetails.code}</p></div>) },
     {
       id: 3,
       tag: (
-        <div style={{ fontSize: "10px" }}>
-    Countries:{" "}
-    <ul style={{ listStyle: "none" }}>
-      {countries
-          |> map((country) => (
-        <li key={prop("code", country)}>{prop("name", country)}</li>
-      ))}
-    </ul>
-  </div>)}]
+        <div style={{ border: "1px solid blue", margin: 2 }}>
+          <div style={{ fontSize: "10px" }}>
+            Countries:{" "}
+            <ul style={{ listStyle: "none" }}>
+              {countries
+                |> map((country) => (
+                <li key={prop("code", country)}>{prop("name", country)}</li>
+              ))}
+            </ul>
+          </div>
+        </div>)
+    }];
 
   const [items, setItems] = useState(itemsList);
+  console.log("items", items);
 
-  const [{ isDragging }, drag] = useDrag(() => ({
+  const [{ isDragging }, drag] = useDrag({
     type: "element",
     item: { id: "id" },
     collect: (monitor) => ({
-      isDragging: !!monitor.isDragging(),
-    }),
-  }));
+      isDragging: monitor.isDragging(),
+    })
+  });
 
+  drag(ref);
 
+  console.log("ref", ref.current);
 
   const message = (
     <ContinentContainer>
       <div>
         {items
-          |> map((item) => (
-            <div ref={drag} key={item.id}>
-              {item.tag}
-            </div>
-          ))}
+          |> mapIndexed((item, index) => (
+          <div id={item.id} key={index} ref={ref}>
+            {item.tag}
+            {isDragging && console.log("is dragging:", item.id)}
+          </div>
+        ))}
       </div>
-     </ContinentContainer>
+    </ContinentContainer>
   );
 
   return (
